9f89db8c559e2321f927070e21c6107c2d7a7560
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-04-11 08:30:56 +0200

    Start version 0.6

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
 
 setup(
         name='tilecloud-chain',
-        version='0.5',
+        version='0.6',
         description='tilecloud chain',
         long_description=README,
         classifiers=[
