b7f83444b5cc54b480f41bd6c6a7bbbf6f492e5a
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-11-03 13:44:08 +0100

    Add process task

--- tilecloud_chain/tests/test_copy.py
+++ tilecloud_chain/tests/test_copy.py
@@ -18,9 +18,6 @@ class TestGenerate(CompareCase):
         if os.path.exists('/tmp/tiles'):
             shutil.rmtree('/tmp/tiles')
         os.makedirs('/tmp/tiles/src/1.0.0/point_hash/default/21781/0/0/')
-        f = open('/tmp/tiles/src/1.0.0/point_hash/default/21781/0/0/0.png', 'w')
-        f.write('test image')
-        f.close()
 
     @classmethod
     def tearDownClass(self):
@@ -32,13 +29,19 @@ class TestGenerate(CompareCase):
     @attr(general=True)
     @log_capture('tilecloud_chain', level=30)
     def test_copy(self, l):
+        f = open('/tmp/tiles/src/1.0.0/point_hash/default/21781/0/0/0.png', 'w')
+        f.write('test image')
+        f.close()
+
         for d in ('-d', '-q', '-v'):
             self.assert_cmd_equals(
                 cmd='./buildout/bin/generate_copy %s -c tilegeneration/test-copy.yaml src dst' % d,
                 main_func=copy_.main,
                 regex=True,
                 expected="""The tile copy of layer 'point_hash' is finish
-Nb copyed tiles: 1
+Nb copy tiles: 1
+Nb errored tiles: 0
+Nb dropped tiles: 0
 Total time: 0:00:[0-9][0-9]
 Total size: 10 o
 Time per tiles: [0-9]+ ms
@@ -54,3 +57,47 @@ Size per tile: 10 o
         f = open('/tmp/tiles/dst/1.0.0/point_hash/default/21781/0/0/0.png', 'r')
         self.assertEquals(f.read(), 'test image')
         f.close()
+
+    @attr(process=True)
+    @attr(general=True)
+    @log_capture('tilecloud_chain', level=30)
+    def test_process(self, l):
+        import subprocess
+        for d in ('-vd', '-q', '-v', ''):
+            subprocess.call([
+                'wget',
+                'http://localhost/mapserv?STYLES=default&SERVICE=WMS&FORMAT=\
+image%2Fpng&REQUEST=GetMap&HEIGHT=256&WIDTH=256&VERSION=1.1.1&BBOX=\
+%28560800.0%2C+158000.0%2C+573600.0%2C+170800.0%29&LAYERS=point&SRS=epsg%3A21781',
+                '-O',
+                '/tmp/tiles/src/1.0.0/point_hash/default/21781/0/0/0.png',
+            ])
+            statinfo = os.stat(
+                '/tmp/tiles/src/1.0.0/point_hash/default/21781/0/0/0.png',
+            )
+            self.assertEquals(statinfo.st_size, 758)
+
+            self.assert_cmd_equals(
+                cmd='./buildout/bin/generate_process %s -c tilegeneration/test-copy.yaml --cache src optipng' % d,
+                main_func=copy_.process,
+                regex=True,
+                expected="""The tile process of layer 'point_hash' is finish
+Nb process tiles: 1
+Nb errored tiles: 0
+Nb dropped tiles: 0
+Total time: 0:00:[0-9][0-9]
+Total size: 103 o
+Time per tiles: [0-9]+ ms
+Size per tile: 103 o
+
+""" if d != '-q' else '',
+                empty_err=True)
+            statinfo = os.stat(
+                '/tmp/tiles/src/1.0.0/point_hash/default/21781/0/0/0.png',
+            )
+            self.assertEquals(statinfo.st_size, 103)
+        l.check(
+            ('tilecloud_chain', 'ERROR', 'The tile: not defined is empty'),
+            ('tilecloud_chain', 'ERROR', 'The tile: not defined is empty'),
+            ('tilecloud_chain', 'ERROR', 'The tile: not defined is empty')
+        )
