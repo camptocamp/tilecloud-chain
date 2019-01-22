05da81cb7977225f21649cc16c5610bd1abc5176
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2019-01-22 15:16:22 +0100

    Release 1.9.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='tilecloud-chain',
-    version='1.9.2',
+    version='1.9.3',
     description=(
         "Tools to generate tiles from WMS or Mapnik, to S3, "
         "Berkley DB, MBTiles, or local filesystem in WMTS layout using "
