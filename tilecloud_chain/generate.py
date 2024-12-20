bff3d5e26e80128a74aa870cce652ade0352247a
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-12-20 10:48:46 +0100

    Add more str and repr methods

--- tilecloud_chain/generate.py
+++ tilecloud_chain/generate.py
@@ -77,6 +77,12 @@ class LogTilesContext:
 
         return tile
 
+    def __str__(self) -> str:
+        return self.__class__.__name__
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
 
 class Generate:
     """Generate the tiles, generate the queue, ..."""
