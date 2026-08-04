4a3a891e94e50abb955672fe77b9fe299c12f931
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-08-04 04:09:12 +0000

    Update ci config for c2cciutils 1.4.21 and fix prettier formatting

--- tilecloud_chain/tests/create_test_data.sh
+++ tilecloud_chain/tests/create_test_data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-
-
 createdb -E UTF8 -T template0 tests-deploy
 psql -q -d tests-deploy -c "CREATE TABLE test (name varchar(10));"
 psql -q -d tests-deploy -c "INSERT INTO test VALUES ('referance');"
